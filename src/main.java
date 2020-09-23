5cea31bfd32286afff0a1fc82c36d08c1591f70d
Robert-Beane <beane031@morris.umn.edu>
2020-09-22 20:09:25 -0500

    Add comments

--- src/main.java
+++ src/main.java
@@ -9,14 +9,13 @@ public class main {
             int compProg;
             int k = 0;
             int j = 0;
-            int pref;
 
-            System.out.println("Input your n amount of companies/programmers.");
+            System.out.println("Input the value n for the number of n companies and n programmers.");
             compProg = scan.nextInt();
-            int[][] comp = new int[compProg][compProg];
-            int[][] prog = new int[compProg][compProg];
+            int[][] comp = new int[compProg][compProg]; // creates the 3d array for the companies based on size given above
+            int[][] prog = new int[compProg][compProg]; // creates the 3d array for the programmers based on size given above
 
-            for (int i = 0; i < compProg; i++){
+            for (int i = 0; i < compProg; i++){ //takes in the preferences for each programmer and puts it into the the prog array.
                 System.out.println("Please input preferences (separated by spaces) for programmer "+ (i+1) +": ");
                 while ( j < compProg){
                     prog[i][k] = scan.nextInt();
@@ -26,7 +25,7 @@ public class main {
                 j=0;
                 k=0;
             }
-            for (int i =0; i < compProg; i++){
+            for (int i =0; i < compProg; i++){ //takes in the preferences for each companies and puts it into the the comp array.
                 System.out.println("Please input preferences (separated by spaces) for company "+ (i+1) +": ");
                 while ( j < compProg){
                     comp[i][k] = scan.nextInt();
